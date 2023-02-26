0072a1c00801aa399fa57e6d7687555639294f08
Солнце <@LAPTOP-DPKNJGCI>
2023-02-26 22:29:11 +0300

    Добавьте файлы проекта.

---
+++ CM_23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CM_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            FactorAsync(n);
+            Console.ReadKey();
+        }
+
+        static void Factor(int n)
+        {
+            int s = 1;
+            for (int i = 1; i < n; i++)
+            {
+                s *= i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine(s);
+        }
+        static async void FactorAsync(int n)
+        {
+            await Task.Run(() => Factor(n));
+        }
+    }
+}
